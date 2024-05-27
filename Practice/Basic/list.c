938005761c2ed9fdb80d18205353f725ab1350c3
PANdas <krsy0411@naver.com>
2024-05-27 18:02:39 +0900

    리스트에 대한 간단한 개념 작성

---
+++ Practice/Basic/list.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 배열 : 생성 시점에 반드시 배열 크기를 지정, 생성한 후에는 크기 변경 불가(단점)
+
+    // 리스트 : 데이터 집합 보관 기능을 가지면서도, 유연하게 크기를 조정 가능한 자료구조
+    // 다양한 자료구조의 기반이 됨(stack, queue, tree, etc..)
+    // linked-list, dobule-linked-list, circular-linked-list
+    
+    return  0;
+}
